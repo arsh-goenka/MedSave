503d9055adc72f468f7ee8cabf66c9d33c8ec344
Israel Ogwu <57731260+israelo19@users.noreply.github.com>
2025-04-12 11:59:48 -0400

    home page

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -4,10 +4,13 @@ import './NavBar.css';
 const NavBar = () => {
   return (
     <nav className="navbar">
-      <div className="navbar-logo">Waste-not-Rx</div>
+      <div className="navbar-logo">WASTE-not-RX</div>
       <ul className="navbar-links">
         <li><a href="#home">Home</a></li>
+        <li><a href="#pharmacy">Pharmacy</a></li>
         <li><a href="#nonprofit">NonProfit</a></li>
+
+        
       </ul>
     </nav>
   );
